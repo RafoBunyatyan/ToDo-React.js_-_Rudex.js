6483ad8c5c97191a0d9bb49aa76bd9d34c387ebd
RafoBunyatyan <rafobuniatyan@mail.ru>
2022-10-20 18:19:54 +0400

    add todos & todos.text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from 'react'
 import './App.css';
+import TodoList from './TodoList';
 
 function App() {
+
+	const [todos, setTodos] = useState([
+		{
+			id: Math.random(),
+			text: 'Learn JS',
+			isCompleted: false,
+		},
+		{
+			id: Math.random(),
+			text: 'Learn CSS',
+			isCompleted: false,
+		},
+		{
+			id: Math.random(),
+			text: 'Learn React',
+			isCompleted: false,
+		},
+	])
+
 	return (
 		<div className="App">
-			Hello Armenia
+			<TodoList todos={todos} />
 		</div>
 	);
 }
