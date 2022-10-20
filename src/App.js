7f9525d315fd89b84bb5b6f421af39067fef3d50
RafoBunyatyan <rafobuniatyan@mail.ru>
2022-10-20 19:08:00 +0400

    add onChange todos.map

--- src/App.js
+++ src/App.js
@@ -40,9 +40,14 @@ function App() {
 				onDelete={(todo) => {
 					setTodos(todos.filter((t) => t.id !== todo.id))
 				}}
-			// onChange={() => {
-
-			// }}
+				onChange={(newTodo) => {
+					setTodos(todos.map((todo) => {
+						if (todo.id === newTodo.id) {
+							return newTodo
+						}
+						return todo
+					}))
+				}}
 			/>
 			<TodoFooter todos={todos} onClearCompleted={() => {
 				setTodos(todos.filter((todo) => !todo.isCompleted))
